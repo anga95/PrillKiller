6a9835058700642a827c3c889ccc268423177030
anga95 <andreas.gabrielsson@live.se>
2024-10-07 19:48:45 +0200

    Initial Commit

---
+++ PrillKillerTests/PrillKillerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrillKillerTests.swift
+//  PrillKillerTests
+//
+//  Created by AnGa on 2024-10-07.
+//
+
+import Testing
+@testable import PrillKiller
+
+struct PrillKillerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
